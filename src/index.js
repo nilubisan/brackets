a25d6f5037ccd3a3639c9364db283bc6bd677f6f
nilubisan <r.nas9329@gmail.com>
2020-10-21 23:05:23 +0600

    feat: complete bracket task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  bracketsConfig = bracketsConfig.flat().join("");
+  let storage = [];
+  for(let i = 0; i < str.length; i++) {
+    if(str[i] == '|' && str[storage[storage.length-1]] == '|') {
+      storage.pop();
+      continue;
+    }
+    let indexOfBracket = bracketsConfig.indexOf(str[i]);
+    if(indexOfBracket%2 === 0) storage.push(indexOfBracket+1);
+    else {
+      if(storage.pop() != bracketsConfig.indexOf(str[i]))  {
+        return false;
+    } 
+  }
+  }
+  return storage.length === 0 ? true : false;
 }
+
+
