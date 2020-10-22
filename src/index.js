510f6859b7dd814d7fcd90a7e69441d0db912800
nilubisan <r.nas9329@gmail.com>
2020-10-22 21:46:24 +0600

    fix: change algorithm for the same opening and closing bracket

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
+  let except = [];
   bracketsConfig = bracketsConfig.flat().join("");
+  
+  for(let i=0; i < bracketsConfig.length; i=i+2) {
+    if(bracketsConfig[i] == bracketsConfig[i+1]) {
+      except.push(i);
+    }
+  }
   let storage = [];
   for(let i = 0; i < str.length; i++) {
-    if(str[i] == '|' && str[storage[storage.length-1]] == '|') {
+    let indexOfBracket = bracketsConfig.indexOf(str[i]);    
+    if(except.includes(indexOfBracket) && indexOfBracket+1 === storage[storage.length-1]) {
       storage.pop();
       continue;
-    }
-    let indexOfBracket = bracketsConfig.indexOf(str[i]);
+    }  
     if(indexOfBracket%2 === 0) storage.push(indexOfBracket+1);
     else {
       if(storage.pop() != bracketsConfig.indexOf(str[i]))  {
